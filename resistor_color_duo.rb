7d13423de74c474807addd0133bd96b505d9b7b9
Ernest <ernestlphillips@gmail.com>
2021-08-10 16:14:25 -0700

    Fimal solution

--- resistor_color_duo.rb
+++ resistor_color_duo.rb
@@ -6,3 +6,20 @@ To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-duo` directory.
 =end
 
+
+
+
+    class ResistorColorDuo
+        RESISTOR_COLORS = ["black",  "brown" , "red" , "orange" , "yellow" , "green"  ,"blue" , "violet" , "grey"  ,"white"]
+        def self.value(colors)
+            a = colors[0]
+            b = colors[1]
+            # puts a + ',' + b
+            firstVal = RESISTOR_COLORS.index(a)
+            secVal = RESISTOR_COLORS.index(b)
+            
+            return "#{firstVal}#{secVal}".to_i
+        end
+    end
+
+    exercism submit resistor_color_duo.rb
